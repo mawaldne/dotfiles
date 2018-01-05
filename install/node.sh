591ca4e1cd1787b83ce5aee377118cef14ea5c30
Mike Waldner <mawaldne@Evas-MacBook-Pro.local>
2018-01-05 13:27:36 -0700

    Fixes

--- install/node.sh
+++ install/node.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo 'Installing Node'
-nvm install v7.2.0
-nvm alias default v7.2.0
+nvm install v7.10.1
+nvm alias default v7.10.1
 
 echo 'Installing global node modules'
 npm install -g yarn eslint_d flow-bin tern
