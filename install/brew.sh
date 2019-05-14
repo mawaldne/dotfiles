c1454dccfbf7b12090e22a61596868ed2c72a7e4
Mike Waldner <mike.waldner@gmail.com>
2019-05-13 20:39:55 -0600

    reattach-to-user-namespace

--- install/brew.sh
+++ install/brew.sh
@@ -2,14 +2,13 @@ echo 'Installing homebrew'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo 'Installing necessary applications'
-brew install git fzf bash-completion tmux ag hub ansible
+brew install git fzf bash-completion tmux ag ansible reattach-to-user-namespace
 
 echo 'Installing oh-my-zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo 'Installing Neovim'
 brew install neovim
-cd ~/.config && ln -s ~/.vim nvim
 
 echo 'Installing Plug - Neovim and Vim'
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
