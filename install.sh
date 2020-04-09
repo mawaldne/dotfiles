185d85c489cd00af197e6cb2a3593d566be5c9b6
Mike Waldner <mike.waldner@gmail.com>
2020-04-09 16:27:04 -0600

    Install

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@
 # 7) Install file hosting (todo: brew command)
 # 8) Install MacVim (brew version doesn't install in applications :( )
 # 9) Install iterm2 and point to iterm2-settings folder (todo: brew command)
+#    a) Preferences - General -> Preferences - Load preferences from a custom folder
 # 10) Install ssh keys
 # 11) Clone dotfiles
-# 12) Install dark mode slack - https://github.com/LanikSJ/slack-dark-mode
 
 source install/dotfiles.sh
 source install/brew.sh
