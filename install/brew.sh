6878e6dc8e23be6089123f094128059131cf36a5
Michael Waldner <michael.waldner@allergan.com>
2024-08-26 09:45:25 -0600

    fix: remove stuff

--- install/brew.sh
+++ install/brew.sh
@@ -5,5 +5,5 @@ echo 'Installing oh-my-zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo 'Installing necessary applications'
-brew install git fzf bash-completion tmux tree rg jq pgFormatter neovim diff-so-fancy
+brew install git fzf bash-completion tmux tree rg jq pgFormatter neovim diff-so-fancy reattach-to-user-namespace
 
