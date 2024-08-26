9cf14eda983b5b9e276d9b5b33f8c81922774b50
Michael Waldner <michael.waldner@allergan.com>
2024-08-26 10:18:56 -0600

    fix: fix

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -6,6 +6,7 @@ rm -f ~/.profile && ln -s dotfiles/.profile ~/.profile
 rm -f ~/.tmux.conf && ln -s dotfiles/.tmux.conf ~/.tmux.conf
 rm -f ~/.vimrc && ln -s dotfiles/.vimrc ~/.vimrc
 rm -f ~/.zshrc && ln -s dotfiles/.zshrc ~/.zshrc
+rm -f ~/.zprofile && ln -s dotfiles/.zprofile ~/.zprofile
 rm -rf ~/.vim && ln -s dotfiles/.vim ~/.vim
 rm -f ~/.config/nvim/init.vim && ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 rm -f ~/.config/wezterm && ln -s ../dotfiles/wezterm/.config/wezterm ~/.config/wezterm
